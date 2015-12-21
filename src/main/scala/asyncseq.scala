47924cb3d9ee2851e47b27cde34b165496ee6de2
Dale Wijnand <dale.wijnand@gmail.com>
2015-12-21 10:37:52 +0000

    Explorations of models to implement flatMap/flatten

--- src/main/scala/asyncseq.scala
+++ src/main/scala/asyncseq.scala
@@ -2,34 +2,175 @@ package asyncseq
 
 import scala.concurrent.{ ExecutionContext => EC, Future }
 
-sealed abstract class AsyncSeq[A] {
-  def map[B](f: A => B)(implicit ec: EC): AsyncSeq[B] =
+// TODO: AnyVal, toString, final, sealed
+
+// 1. singly linked list
+// 2. value is a future value
+// 3. once computed it could be empty
+// 4. but when present it's: a value and optionally a next page value
+// 5. the next page is the next cell
+// list termination is either:
+// * locally at #4
+// * or remotely at #3
+
+final case class FList[+A](value: Future[Option[(A, Option[FList[A]])]]) {
+  def map[B](f: A => B)(implicit ec: EC): FList[B] = {
+    FList(value flatMap {
+      case None                     => Future successful None
+      case Some((head, None))       => Future successful Some((f(head), None))
+      case Some((head, Some(tail))) => Future successful Some((f(head), Some(tail map f)))
+    })
+  }
+
+  /*
+  flatten (us1 :: us2 :: FNil) ::
+          (uk1 :: uk2 :: FNil) ::
+          (au1 :: au2 :: FNil) ::
+          FNil
+       to us1 :: us2 :: uk1 :: uk2 :: au1 :: au2 :: FNil
+   */
+  def flatten[B](implicit ec: EC, ev: A <:< FList[B]): FList[B] = {
+    def inner(head: B, optTail: Option[FList[B]], finalTail: Option[FList[B]]): Future[Option[(B, Option[FList[B]])]] = {
+      optTail match {
+        case None       => Future successful Some((head, finalTail))
+        case Some(tail) =>
+          tail.value flatMap {
+            case None            => Future successful Some((head, finalTail))
+            case Some((h, optT)) => Future successful Some((head, Some(FList(inner(h, optT, finalTail)))))
+          }
+      }
+    }
+    def loop(target: FList[FList[B]]): Future[Option[(B, Option[FList[B]])]] = {
+      target.value flatMap {
+        case None                      => Future successful None
+        case Some((xsHead, xsOptTail)) =>
+          xsHead.value flatMap {
+            case None               =>
+              xsOptTail match {
+                case None         => Future successful None
+                case Some(xsTail) => loop(xsTail)
+              }
+            case Some((head, optTail)) => inner(head, optTail, xsOptTail map (_.flatten))
+          }
+      }
+    }
+    FList(loop(this map ev))
+  }
+}
+
+sealed trait FutureList[+A] extends Product with Serializable {
+  def headOption(implicit ec: EC): Future[Option[A]] =
     this match {
-      case Last(a)     => Last(f(a))
-      case Cons(a, ft) => Cons(f(a), ft map (_ map f))
+      case FutureNil         => Future successful None
+      case FutureCons(h, _) => h map (Some(_))
     }
 
-  def flatMap[B](f: A => AsyncSeq[B])(implicit ec: EC): AsyncSeq[B] =
+  def tailOption(implicit ec: EC): Future[Option[FutureList[A]]] =
     this match {
-      case Last(a)     => f(a)
-      case Cons(a, ft) => f(a) concat (ft map (_ flatMap f))
+      case FutureNil        => Future successful Some(FutureNil)
+      case FutureCons(_, t) => t map (Some(_))
     }
 
-  def concat(rhs: Future[AsyncSeq[A]])(implicit ec: EC): AsyncSeq[A] =
+  def map[B](f: A => B)(implicit ec: EC): FutureList[B] =
     this match {
-      case Last(a)     => Cons(a, rhs)
-      case Cons(a, ft) => Cons(a, ft map (_ concat rhs))
+      case FutureNil        => FutureNil
+      case FutureCons(h, t) => FutureCons(h map f, t map (_ map f))
+    }
+
+  def flatMap[B](f: A => FutureList[B])(implicit ec: EC): FutureList[B] =
+    this match {
+      case FutureNil        => FutureNil
+      case FutureCons(h, t) => map(f).flatten
+    }
+
+  def flatten[B](implicit ec: EC, ev: A <:< FutureList[B]): FutureList[B] = {
+    val target: FutureList[FutureList[B]] = this map ev
+    target match {
+      case FutureNil                  => FutureNil
+      case FutureCons(xsHead, xsTail) =>
+//        valueList flatMap {
+//          case (FutureNil, tailList)         => tailList.flatten
+//          case (FutureCons(value), tailList) =>
+//            value map { case (head, tail) =>
+//              FutureCons(tailList.flatten map (tail2 => (head, tail ++ tail2)))
+//            }
+//        }
+        ??? : FutureList[B]
+    }
+  }
+
+  def ++[A1 >: A](that: FutureList[A1])(implicit/**/ ec: EC): FutureList[A1] =
+    this match {
+      case FutureNil        => that
+      case FutureCons(h, t) => FutureCons(h, t map (_ ++ that))
     }
 }
 
-// TODO: AsyncCons? Async.Cons? Cons?
-final case class Cons[A](value: A, tail: Future[AsyncSeq[A]]) extends AsyncSeq[A]
-final case class Last[A](value: A)                            extends AsyncSeq[A]
+final case class FutureCons[A](head: Future[A], tail: Future[FutureList[A]]) extends FutureList[A]
 
-object AsyncSeq {
-  def apply[A](start: A, call: A => Option[Future[A]])(implicit ec: EC): AsyncSeq[A] =
-    call(start) match {
-      case Some(fa) => Cons(start, fa.map(a => AsyncSeq(a, call)))
-      case None     => Last(start)
-    }
+sealed trait FutureNil extends FutureList[Nothing] {
+  override def toString = "FutureNil"
 }
+
+final case object FutureNil extends FutureNil
+
+//final case class FutureList[A](value: Future[Page[A]]) {
+//  def head(implicit ec: EC): Future[A] = value map (_.head)
+//
+//  def tail(implicit ec: EC): Future[Option[FutureList[A]]] = value map (p => p.tail map (FutureList(_)))
+//
+//  def map[B](f: A => B)(implicit ec: EC): FutureList[B] = FutureList(value map (_ map f))
+//
+//  def flatMap[B](f: A => FutureList[B])(implicit ec: EC): FutureList[B] = {
+//    FutureList(
+//      value flatMap {
+//        case ConsPage(a, ft) => f(a).value map (_ concat ft.map(_ map f))
+//        case LastPage(a)     => f(a).value
+//      }
+//    )
+//    ???
+//  }
+//
+//  def concat(rhs: Future[Page[A]])(implicit ec: EC): Page[A] =
+//    ???
+//}
+
+//final case class AsyncSeq[A](value: Future[(A, Option[AsyncSeq[A]])]) {
+//  def head(implicit ec: EC): Future[A]                   = value map (_._1)
+//  def tail(implicit ec: EC): Future[Option[AsyncSeq[A]]] = value map (_._2)
+//
+//  def map[B](f: A => B)(implicit ec: EC): AsyncSeq[B] =
+//    AsyncSeq(value map { case (head, tail) => (f(head), tail map (xs => xs map f)) })
+//
+//  def flatMap[B](f: A => AsyncSeq[B])(implicit ec: EC): AsyncSeq[B] = this.map(f).flatten
+//
+//  def flatten[B](implicit ec: EC, ev: A <:< AsyncSeq[B]): AsyncSeq[B] = {
+//    val xs: AsyncSeq[AsyncSeq[B]] = this map ev
+//    AsyncSeq {
+//      val origValue: Future[(AsyncSeq[B], Option[AsyncSeq[AsyncSeq[B]]])] = xs.value
+//      origValue flatMap { case (origHead: AsyncSeq[B], origTail: Option[AsyncSeq[AsyncSeq[B]]]) =>
+//        val origHeadValue: Future[(B, Option[AsyncSeq[B]])] = origHead.value
+//        origHeadValue map {
+//          case (b: B, None) => (b, origTail)
+//          case (b: B, Some(tail2: AsyncSeq[B])) =>
+//            (b,
+//              tail2
+//              )
+//        }
+//        ??? : Future[(B, Option[AsyncSeq[B]])]
+//      }
+//      ??? : Future[(B, Option[AsyncSeq[B]])]
+//    }
+//  }
+//
+//  //  def concat(rhs: Future[AsyncSeq[A]])(implicit ec: EC): AsyncSeq[A] =
+////    this match {
+////      case Last(a)     => Cons(a, rhs)
+////      case Cons(a, ft) => Cons(a, ft map (_ concat rhs))
+////    }
+//}
+//
+//object AsyncSeq {
+//  def unpag[A](head: Future[A], call: A => Option[Future[A]])(implicit ec: EC): AsyncSeq[A] =
+//    AsyncSeq(head map (a => (a, call(a) map (fa => unpag(fa, call)))))
+//}
--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -7,41 +7,51 @@ import scala.concurrent.{ Await, Future }
 import scala.io.AnsiColor._
 
 object Test {
-  def alloc(n: Int): Int = {
-    println(s"$CYAN[2]$RESET allocating $n...")
-    Thread sleep 10 // simulating some time to do some IO
-    println(s"$CYAN[2]$RESET allocated  $n")
-    n
+  val   allocSleep = 800L // simulating some time to do some IO
+  val inspectSleep = 2200L // a longer time to test that seeding, mapping & flat-mapping happens asynchronously
+
+  def isoTime() = {
+    val df = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    df setTimeZone (java.util.TimeZone getTimeZone "UTC")
+    df format new java.util.Date()
   }
 
+  def alloc(n: Int): Future[Int] =
+    Future {
+      println(s"$isoTime $CYAN[2]$RESET allocating $n...")
+      Thread sleep allocSleep
+      println(s"$isoTime $CYAN[2]$RESET allocated  $n")
+      n
+    }
+
   def createPayload: AsyncSeq[Int] = {
-    println(s"$WHITE[1]$RESET creating AsyncSeq...")
-    val payload = AsyncSeq[Int](alloc(0), n => if (n < 9) Some(Future(alloc(n + 1))) else None)
-    println(s"$WHITE[1]$RESET created  AsyncSeq")
+    println(s"$isoTime $WHITE[1]$RESET creating AsyncSeq...")
+    val payload = AsyncSeq.unpag[Int](alloc(0), n => if (n < 9) Some(alloc(n + 1)) else None)
+    println(s"$isoTime $WHITE[1]$RESET created  AsyncSeq")
     payload
   }
 
-  @tailrec def inspectPayload[A](p: AsyncSeq[A]): Unit =
-    p match {
-      case Last(n)     =>
-        println(s"$YELLOW[4]$RESET saw $n (last)")
-      case Cons(n, ft) =>
-        println(s"$YELLOW[4]$RESET saw $n")
-        Thread sleep 80 // a longer time to test that seeding, mapping & flat-mapping happens asynchronously
-        inspectPayload(Await.result(ft, 2.seconds))
+  @tailrec def inspectPayload[A](p: AsyncSeq[A]): Unit = {
+    Await.result(p.value, 2.seconds) match {
+      case (a, Some(tail)) =>
+        println(s"$isoTime $YELLOW[4]$RESET saw $a")
+        Thread sleep inspectSleep
+        inspectPayload(tail)
+      case (a, None) => println(s"$isoTime $YELLOW[4]$RESET saw $a (last)")
     }
+  }
 
   def main(args: Array[String]): Unit = {
-    inspectPayload(createPayload)
-    println()
-    inspectPayload(createPayload map { n =>
-      println(s"$MAGENTA[3]$RESET transforming $n to Double")
-      n.toDouble
-    })
-    println()
-    inspectPayload(createPayload flatMap { n =>
-      println(s"$MAGENTA[3]$RESET duplicating $n into two")
-      Cons(n, Future(Last(n)))
-    })
+//    inspectPayload(createPayload)
+//    println()
+//    inspectPayload(createPayload map { n =>
+//      println(s"$isoTime $MAGENTA[3]$RESET transforming $n to Double")
+//      n.toDouble
+//    })
+//    println()
+//    inspectPayload(createPayload flatMap { n =>
+//      println(s"$isoTime $MAGENTA[3]$RESET duplicating $n into two")
+//      Cons(n, Future(Last(n)))
+//    })
   }
 }
