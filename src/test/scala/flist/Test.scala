e9fb9f19638852eaf375d6ebdedb69db2f687122
Dale Wijnand <dale.wijnand@gmail.com>
2015-12-22 23:08:22 +0000

    Fixup flist.Test

--- src/test/scala/flist/Test.scala
+++ src/test/scala/flist/Test.scala
@@ -1,4 +1,4 @@
-package asyncseq
+package flist
 
 import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits._
@@ -7,8 +7,9 @@ import scala.concurrent.{ Await, Future }
 import scala.io.AnsiColor._
 
 object Test {
-  val   allocSleep = 800L // simulating some time to do some IO
-  val inspectSleep = 2200L // a longer time to test that seeding, mapping & flat-mapping happens asynchronously
+  val     maxAlloc = 100
+  val   allocSleep = 10L // simulating some time to do some IO
+  val inspectSleep = 80L // a longer time to test that seeding, mapping & flat-mapping happens asynchronously
 
   def isoTime() = {
     val df = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
@@ -16,28 +17,28 @@ object Test {
     df format new java.util.Date()
   }
 
-  def alloc(n: Int): Future[Int] =
+  def alloc(n: Int): Future[Option[Int]] =
     Future {
       println(s"$isoTime $CYAN[2]$RESET allocating $n...")
       Thread sleep allocSleep
       println(s"$isoTime $CYAN[2]$RESET allocated  $n")
-      n
+      Some(n)
     }
 
-  def createPayload: AsyncSeq[Int] = {
+  def createPayload: FList[Int] = {
     println(s"$isoTime $WHITE[1]$RESET creating AsyncSeq...")
-    val payload = AsyncSeq.unpag[Int](alloc(0), n => if (n < 9) Some(alloc(n + 1)) else None)
+    val payload = FList.iterate[Int](alloc(0))(n => if (n < maxAlloc) alloc(n + 1) else Future successful None)
     println(s"$isoTime $WHITE[1]$RESET created  AsyncSeq")
     payload
   }
 
-  @tailrec def inspectPayload[A](p: AsyncSeq[A]): Unit = {
-    Await.result(p.value, 2.seconds) match {
-      case (a, Some(tail)) =>
+  @tailrec def inspectPayload[A](p: FList[A]): Unit = {
+    Await.result(p.value.value, 2.seconds) match {
+      case Some((a, tail)) =>
         println(s"$isoTime $YELLOW[4]$RESET saw $a")
         Thread sleep inspectSleep
         inspectPayload(tail)
-      case (a, None) => println(s"$isoTime $YELLOW[4]$RESET saw $a (last)")
+      case None => println(s"$isoTime $YELLOW[4]$RESET saw last")
     }
   }
 
@@ -51,7 +52,9 @@ object Test {
 //    println()
 //    inspectPayload(createPayload flatMap { n =>
 //      println(s"$isoTime $MAGENTA[3]$RESET duplicating $n into two")
-//      Cons(n, Future(Last(n)))
+//      FList(FutureOption(Future(Some((n,
+//        FList(FutureOption(Future(Some((n,
+//          FList(FutureOption(Future(None)))))))))))))
 //    })
   }
 }
