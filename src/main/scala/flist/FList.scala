90c71ca87718d00e1900c4893e808f224c9b4591
Dale Wijnand <dale.wijnand@gmail.com>
2015-12-23 10:32:12 +0000

    Add some initial conversions

--- src/main/scala/flist/FList.scala
+++ src/main/scala/flist/FList.scala
@@ -1,6 +1,9 @@
 package flist
 
 import scala.annotation.tailrec
+import scala.annotation.unchecked.{ uncheckedVariance => uV }
+import scala.collection.generic.{ CanBuildFrom => CBF }
+import scala.collection.mutable
 import scala.concurrent.{ ExecutionContext => EC, Future }
 import scala.util.{ Failure, Success }
 
@@ -46,6 +49,17 @@ final case class FList[+A](value: FutureOption[(A, FList[A])]) {
   // Specific Folds
   def flatten[B](implicit ec: EC, ev: A <:< FList[B]): FList[B] =
     FList(this.map(ev).value flatMap { case (h, t) => (h ++ t.flatten).value })
+
+  // Conversions
+  private def fromBuilder[A1 >: A, CC[_]](b: mutable.Builder[A1, CC[A1]])(implicit ec: EC): Future[CC[A1]] =
+    foldLeft(b)(_ += _).map(_.result)
+
+  def to[Col[_]](implicit cbf: CBF[Nothing, A, Col[A @uV]], ec: EC): Future[Col[A @uV]] = fromBuilder(cbf())
+
+  def toVector(implicit ec: EC): Future[Vector[A]] = to[Vector]
+
+  def toMap[K, V](implicit ec: EC, ev: A <:< (K, V)): Future[Map[K, V]] =
+    foldLeft(Map.newBuilder[K, V])(_ += _) map (_.result)
 
   // Strings
   def addString(b: StringBuilder, start: String, sep: String, end: String): StringBuilder = {
